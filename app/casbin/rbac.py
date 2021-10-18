ea004ed6c72c797b623c412ed5739cc806249e75
yijunxu <yijunx@umich.edu>
2021-10-18 15:39:11 +0000

    added admin user id and role id for casbin for test

--- app/casbin/rbac.py
+++ app/casbin/rbac.py
@@ -1,7 +1,8 @@
+from casbin import enforcer
 import casbin_sqlalchemy_adapter
 import casbin
 from app.config.app_config import conf
-from role_definition import RolesEnum
+from role_definition import resource_right_action_mapping, ResourceActionsEnum
 
 
 adapter = casbin_sqlalchemy_adapter.Adapter(conf.DATABASE_URI)
@@ -9,9 +10,23 @@ casbin_enforcer = casbin.Enforcer("app/casbin/model.conf", adapter)
 
 
 # probably need to come from configurations?
+# now added a function here to 
+RESOURCE = "/items"
+def actions_mapping(action: str, resource_right: str) -> bool:
+    """
+    actions are get download patch share...
+    resource_right are own / edit / view
+    """
+    if resource_right in resource_right_action_mapping:
+        if action in resource_right_action_mapping[resource_right]:
+            return True
+    return False
 
-
-
+casbin_enforcer.add_function("actions_mapping", actions_mapping)
+# add admin role, only admin can create
+casbin_enforcer.add_policy("admin_role_id", RESOURCE, ResourceActionsEnum.create.name)
+# add this user to admin 
+casbin_enforcer.add_grouping_policy("admin_user_id", "admin_role_id")
 
 
 def construct_role_name(item_id: str) -> str:
--- app/casbin/role_definition.py
+++ app/casbin/role_definition.py
@@ -1,11 +1,53 @@
-from enum import Enum
+from enum import Enum, auto
+from pydantic import BaseModel
 
 
-class RolesEnum(Enum):
+class SpecificResourceRightsEnum(Enum):
     """one user can only be one of the below"""
-    viewer = "view"
-    editor = "edit"
-    owner = "own"
+    view = "view"  # user is viewer
+    edit = "edit"  # user is editor
+    own = "own"   # user is owner
+
+
+class ResourceActionsEnum(Enum):
+    """actions to happen on resource/ without id"""
+    get_all = "get_all"
+    create = "create"
+
+
+class SpecificResourceActionsEnum(Enum):
+    """these are the actions to happen on a resource/resource_id"""
+    get = "get"
+    download = "download"
+    patch = "patch"
+    share = "share"
+    unshare = "unshare"
+    delete = "delete"
+    
+
+# this is dynamic
+resource_right_action_mapping: dict = {
+        SpecificResourceRightsEnum.view: {
+            SpecificResourceActionsEnum.get.name,
+            SpecificResourceActionsEnum.download.name
+        },
+        SpecificResourceRightsEnum.edit.name: {
+            SpecificResourceActionsEnum.get.name,
+            SpecificResourceActionsEnum.download.name,
+            SpecificResourceActionsEnum.patch.name
+        },
+        SpecificResourceRightsEnum.own.name: {
+            SpecificResourceActionsEnum.get.name,
+            SpecificResourceActionsEnum.download.name,
+            SpecificResourceActionsEnum.patch.name,
+            SpecificResourceActionsEnum.share.name,
+            SpecificResourceActionsEnum.unshare.name,
+            SpecificResourceActionsEnum.delete.name
+        },
+    }
+
+
+
 
 
 # class ViewActionsEnum(Enum):
